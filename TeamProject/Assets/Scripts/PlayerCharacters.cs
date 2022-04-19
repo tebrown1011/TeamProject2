cc23f5a5afb6e729529b22a03d04009276da3167
PokeSpencerMx <91617455+PokeSpencerMx@users.noreply.github.com>
2022-04-19 13:42:14 -0400

    HorizontalMovementAdded

--- TeamProject/Assets/Scripts/PlayerCharacters.cs
+++ TeamProject/Assets/Scripts/PlayerCharacters.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class PlayerCharacters : MonoBehaviour
 {
-    public int velocity;
+    public int speed;
+    public int gravity;
+
+    public string HorizontalInput;
     
+
+    Rigidbody2D rb2d;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xMove = Input.GetAxisRaw(HorizontalInput);
+        float yMove = Input.GetAxisRaw("Vertical");
+        rb2d.velocity = new Vector2(xMove * speed, -1 * gravity);
     }
 }
