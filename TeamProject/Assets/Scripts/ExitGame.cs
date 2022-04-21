2b0fcb464e5b41a81580cee15426cd1e8288f4a1
NCRaider1 <Nicholas.Centeno.ths15@gmail.com>
2022-04-21 16:18:55 -0400

    AddedTitle

---
+++ TeamProject/Assets/Scripts/ExitGame.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ExitGame : MonoBehaviour
+{
+    // Update is called once per frame
+    public void doExitGame()
+    {
+        Application.Quit();
+    }
+}
